96e410b4072ab84ec81732adacf1eabcacd637d8
aymenbraiek <aymen.braiek@esprit.tn>
2023-03-31 14:34:43 +0200

    add Book test -closes #4

---
+++ src/test/java/com/graphql/graphql/component/Fake/FakeBookDataResolverTest.java
@@ -0,0 +1,62 @@
+package com.graphql.graphql.component.Fake;
+
+import com.graphql.graphql.generated.client.BooksGraphQLQuery;
+import com.graphql.graphql.generated.client.BooksProjectionRoot;
+import com.jayway.jsonpath.TypeRef;
+import com.netflix.dgs.codegen.generated.types.Author;
+import com.netflix.graphql.dgs.DgsQueryExecutor;
+import com.netflix.graphql.dgs.client.codegen.GraphQLQueryRequest;
+import net.datafaker.Faker;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class FakeBookDataResolverTest {
+    @Autowired
+    DgsQueryExecutor dgsQueryExecutor;
+
+    Faker faker;
+
+    @Test
+    void booksWrittenBy() {
+        var graphqlQuery = new BooksGraphQLQuery.Builder().build();
+        var projectionRoot = new BooksProjectionRoot().title()
+                .author().name()
+                .originCountry()
+                .getRoot().released().year();
+        var graphqlQueryRequest = new GraphQLQueryRequest(graphqlQuery, projectionRoot).serialize();
+
+        List<String> titles = dgsQueryExecutor.executeAndExtractJsonPath(
+                graphqlQueryRequest, "data.books[*].title");
+
+        assertNotNull(titles);
+        assertFalse(titles.isEmpty());
+
+        List<Author> authors = dgsQueryExecutor.executeAndExtractJsonPathAsObject(
+                graphqlQueryRequest, "data.books[*].author",
+                new TypeRef<List<Author>>() {
+                }
+        );
+
+        assertNotNull(authors);
+        assertEquals(titles.size(), authors.size());
+
+        List<Integer> releaseYears = dgsQueryExecutor.executeAndExtractJsonPathAsObject(
+                graphqlQueryRequest, "data.books[*].released.year",
+                new TypeRef<List<Integer>>() {
+                }
+        );
+
+        assertNotNull(releaseYears);
+        assertEquals(titles.size(), releaseYears.size());
+    }
+
+    @Test
+    void getBooksByReleased() {
+    }
+}
---
+++ src/test/java/com/graphql/graphql/component/Fake/FakeHelloDataResolverTest.java
@@ -0,0 +1,14 @@
+package com.graphql.graphql.component.Fake;
+
+import com.netflix.graphql.dgs.DgsQueryExecutor;
+import graphql.Assert;
+import org.junit.platform.commons.util.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+class FakeHelloDataResolverTest {
+
+}
